a14e222a040aa061dc85e283e205f6cb30c7c428
106492616 <106492616@student.swin.edu.au>
2026-05-29 11:14:05 +1000

    Organise Css, Complete Index, Complete inc, make images file

--- index.php
+++ index.php
@@ -1,64 +1,28 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="description" content="index page">
-    <meta name="keywords" content="index">
-    <meta name="author" content="jake r">
-    <title>Homepage</title>
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
+<?php include 'includes/header.inc'; ?>
 
-    <nav>
-        <a href="index.php">Homepage</a> 
-        <a href="jobs.php">Job Descriptions</a> 
-        <a href="apply.php">Apply to us</a> 
-        <a href="about.php">About us</a>
-    </nav>
+<body class="index">
 
-    <header>
-        <p class="title">Webber Creative</p>
-        <img src="CompanyLogo.png" alt="Company logo" class="logo">
-        <p class="slogan">Creative ideas, digital impact..</p>
+<?php include 'includes/nav.inc'; ?>
 
-        <div id="borderLine1"></div>
-        <div id="borderLine2"></div>
+<header>
+    <p class="title">Webber Creative</p>
+    <img src="images/CompanyLogo.png" alt="Company logo" class="logo">
+    <p class="slogan">Creative ideas, digital impact...</p>
+    <div id="borderLine1"></div>
+    <div id="borderLine2"></div>
+    <p class="description">
+    Webber Creative is a digital media agency specialising in web design, branding, and digital solutions for modern businesses. 
+    <br><br/>
+    We create engaging online experiences that strengthen brand identity, improve customer interaction, and drive long-term growth.
+    </p>
+</header>
 
-        <p class="description">
-           Webber Creative is a digital media agency specialising in web design and branding for modern online experiences.
-        </p>
-    </header>
+<div class="search-box">
+    <input type="text" placeholder="">
+    <button>Search</button>
+</div>
 
-    <footer>
-        <table class="footer-table">
-            <tr>
-                <td colspan="2">
-                    <a href="https://106492616.atlassian.net/jira/software/projects/QSS/boards/2" target="_blank">
-                        Jira Project Link
-                    </a> |
-                </td>
-                <td colspan="2">
-                    <a href="https://github.com/HengTech282/web-tech-assignment" target="_blank">
-                        GitHub Repository Link
-                    </a> |
-                    <br><br>
-                    <a href="https://hengtech282.github.io/web-tech-assignment/" target="_blank">
-                        Live Website Link
-                    </a>
-                </td>
-                <td colspan="2">
-                    <a href="mailto:info@webber.com">info@webber.com</a>
-                    </a> 
-                </td>
-            </tr>
-        </table>
-    </footer>
-
-    <div class="search-box">
-        <input type="text" placeholder="">
-        <button>Search</button>
-    </div>
+<?php include 'includes/footer.inc'; ?>
 
 </body>
 </html>
